19a261e7adeac046d98a43172f5ef228a9416db7
sohan0898 <shuhanur.rahman08@gmail.com>
2023-09-26 12:12:39 +0600

    added see all button in donation page conditionally

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -5,6 +5,8 @@ const Donation = () => {
   const [doantionCard, setDonationCard] = useState([]);
   const [noFound, setNoFound] = useState(false);
 
+  const [isShow, setIsShow] = useState(false);
+
   useEffect(() => {
     const donationItems = JSON.parse(localStorage.getItem("donation"));
 
@@ -19,13 +21,32 @@ const Donation = () => {
     <div>
       |
       {noFound ? (
-        <h1 className="h-[60vh] text-3xl font-semibold flex justify-center items-center">{noFound}</h1>
+        <h1 className="h-[60vh] text-3xl font-semibold flex justify-center items-center">
+          {noFound}
+        </h1>
       ) : (
         <div className="max-w-screen-2xl mx-auto px-4 md:px-8 lg:px-20 py-4 my-9 ">
           <div className="grid grid-cols-2 gap-5 ">
-            {doantionCard.map((details) => (
-              <DonationCard key={details.id} details={details}></DonationCard>
-            ))}
+            {
+                isShow ? doantionCard.map((details) => (
+                    <DonationCard key={details.id} details={details}></DonationCard>
+                  ))
+
+                  : doantionCard.slice(0, 4).map((details) => (
+                    <DonationCard key={details.id} details={details}></DonationCard>
+                  ))
+
+            }
+          </div>
+
+          <div className="mt-8 text-center">
+            
+            {
+                  doantionCard.length > 4 &&  <button onClick={()=>setIsShow(!isShow)} className="btn text-white normal-case font-semibold bg-[#009444]">
+                {isShow ? "See Less" : "See All"}
+              </button>
+            }
+
           </div>
         </div>
       )}
@@ -34,3 +55,4 @@ const Donation = () => {
 };
 
 export default Donation;
+
